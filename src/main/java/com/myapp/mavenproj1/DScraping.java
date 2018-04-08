69aa893ed6632c013d8d8207ecad65da0f3f58fa
AmanAsanovich <37670139+AmanAsanovich@users.noreply.github.com>
2018-04-08 19:24:06 +0600

    Remove debug files

--- src/main/java/com/myapp/mavenproj1/DScraping.java
+++ src/main/java/com/myapp/mavenproj1/DScraping.java
@@ -83,10 +83,10 @@ public class DScraping {
             
             // For debuging, because data is large - Small states 2, 8, 12-13, 20, 29, 30, 40, 51
             i++;
-            if (i < 29) {
+            if (i < 1) {
                 continue;
             }
-            if (i > 30) {
+            if (i > 1) {
                 break;
             }
             // For debuging, because data is large - Small states 2, 8, 12-13, 20, 29, 30, 40, 51
